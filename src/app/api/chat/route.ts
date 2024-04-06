0d224b4f28d453bbcb0b2fcd693248df506c65e6
Sciederrick <derrickmbarani@gmail.com>
2024-04-06 20:06:07 +0300

    feat: Jokes generator

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -4,6 +4,7 @@ import { OpenAIStream, StreamingTextResponse } from 'ai';
 // Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
+  // baseURL: 'http://127.0.0.1:5000/v1'
 });
  
 // IMPORTANT! Set the runtime to edge
@@ -19,7 +20,7 @@ export async function POST(req: Request) {
     messages: [
       {
         role: 'system',
-        content: 'You are a professional chef. You provide detailed cooking instructions, tips, and advice on selecting the best ingredients.'
+        content: 'You are a jovial character who loves to entertain others with your wit and humor. You have a quick mind and a knack for finding the funny side of any situation, making you a delight to be around. With a seemingly endless supply of jokes, you evoke infectious laughter.'
       },
       ...messages,
     ],
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,34 @@ export default function Chat() {
     append,
   } = useChat();
 
-  const [imageIsLoading, setImageIsLoading] = useState(false);
-  const [image, setImage] = useState<string | null>(null);
+  const [topics] = useState(['work', 'people', 'food', 'television', 'satirical-humor']);
+  const [jokeTypes] = useState(['puns', 'one-liner', 'observational-humor'])
+  const [tones] = useState(['witty', 'sarcastic', 'silly', 'dark', 'goofy'])
+
+  const [temp, setTemp] = useState(0.5);
+  const [topic, setTopic] = useState(topics[0]);
+  const [jokeType, setJokeType] = useState(jokeTypes[0]);
+  const [tone, setTone] = useState(tones[0]);
+
+  const handleTemperatureChange = (e:any) => {
+      setTemp(e.target.value);
+  };
+
+  const handleClickTopic = (choice: string) => {
+    setTopic(choice);
+  };
+
+  const handleClickJokeType = (choice: string) => {
+    setJokeType(choice);
+  };
+
+  const handleClickTone = (choice: string) => {
+    setTone(choice);
+  };
+
+  const handleClickSubmit = () => {
+    append({ role: "user", content: `Generate a ${tone} ${jokeType} joke based on ${topic}` }, { options: { body: {temperature: temp}}})
+  }
 
   const messagesContainerRef = useRef<HTMLDivElement>(null);
 
@@ -22,33 +48,60 @@ export default function Chat() {
     }
   }, [messages]);
 
-  if (imageIsLoading) {
-    return (
-      <div className="flex justify-center items-center h-screen">
-        <div className="loader">
-          <div className="animate-pulse flex space-x-4">
-            <div className="rounded-full bg-slate-700 h-10 w-10"></div>
+  return (
+    <div className="flex flex-col w-full h-screen max-w-[768px] py-12 mx-auto stretch">
+      <div className="p-4">
+        <div className="container mx-auto prose lg:prose-lg xl:prose-xl text-center">
+          <div className="space-y-4">
+            <h1 className="text-4xl font-bold">Generate Jokes</h1>
+            <p className="text-gray-500 dark:text-gray-400">
+              choose the topic, type, tone, and temperature to generate jokes using the Shadcn Joke API.
+            </p>
+          </div>
+          <div className="flex flex-col gap-4 min-h-[400px] justify-center pt-8">
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-4 items-start">
+              <div className="space-y-2">
+                <h3 className="underline">Topic</h3>
+                  <ul className="params-container" id="topic">
+                      {topics.map((mTopic, index) => (
+                          <li 
+                            onClick={() => handleClickTopic(mTopic)}
+                            className={`params-select ${ mTopic === topic ? 'border-green-600' : '' }`}
+                            key={index}>{mTopic}</li>
+                      ))}
+                  </ul>
+              </div>
+              <div className="space-y-2">
+                <h3 className="underline">Type</h3>
+                <ul className="params-container" id="type">
+                  {jokeTypes.map((mJokeType, index) => (
+                      <li 
+                        onClick={() => handleClickJokeType(mJokeType)}
+                        className={`params-select ${ mJokeType === jokeType ? 'border-green-600' : '' }`}
+                        key={index}>{mJokeType}</li>
+                  ))}
+                </ul>
+              </div>
+              <div className="space-y-2">
+                <h3 className="underline">Tone</h3>
+                <ul className="params-container" id="tone">
+                  {tones.map((mTone, index) => (
+                      <li 
+                        onClick={() => handleClickTone(mTone)}
+                        className={`params-select ${ mTone === tone ? 'border-green-600' : '' }`}
+                        key={index}>{mTone}</li>
+                  ))}
+                </ul>
+              </div>
+              <div className="space-y-2">
+                <h3>Temperature</h3>
+                <input className="text-gray-900 px-3 rounded"
+                  id="temperature" max="1" min="0" step="0.1" type="number" value={temp} onChange={handleTemperatureChange}/>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    );
-  }
-
-  if (image) {
-    return (
-      <div className="card w-full h-screen max-w-[768px] py-24 mx-auto stretch">
-        <img src={`data:image/jpeg;base64,${image}`} />
-        <textarea
-          className="mt-4 w-full text-white bg-black h-64"
-          value={messages[messages.length - 1].content}
-          readOnly
-        />
-      </div>
-    );
-  }
-
-  return (
-    <div className="flex flex-col w-full h-screen max-w-[768px] py-24 mx-auto stretch">
       <div className="overflow-auto mb-8 w-full" ref={messagesContainerRef}>
         {messages.map((m) => (
           <div
@@ -72,40 +125,12 @@ export default function Chat() {
       </div>
       <div className="fixed bottom-0 w-full max-w-[768px]">
         <div className="flex flex-col justify-center mb-2 items-center">
-          {messages.length == 0 && (
-            <button
-              className="bg-blue-500 p-2 text-white rounded shadow-xl"
-              disabled={isLoading}
-              onClick={() =>
-                append({ role: "user", content: "Give me a random recipe" })
-              }
-            >
-              Random Recipe
-            </button>
-          )}
-          {messages.length == 2 && !isLoading && (
-            <button
-              className="bg-blue-500 p-2 text-white rounded shadow-xl"
-              disabled={isLoading}
-              onClick={async () => {
-                setImageIsLoading(true);
-                const response = await fetch("api/images", {
-                  method: "POST",
-                  headers: {
-                    "Content-Type": "application/json",
-                  },
-                  body: JSON.stringify({
-                    message: messages[messages.length - 1].content,
-                  }),
-                });
-                const data = await response.json();
-                setImage(data);
-                setImageIsLoading(false);
-              }}
-            >
-              Generate image
-            </button>
-          )}
+          <button
+            className="bg-blue-500 p-2 text-white rounded shadow-xl"
+            disabled={isLoading}
+            onClick={handleClickSubmit}>
+            Generate a joke
+          </button>
         </div>
       </div>
     </div>
